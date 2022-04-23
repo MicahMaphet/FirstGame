f029fb69555b08e58ed0317d83199836c56c30e1
hotpig <93409262+MicahMaphet@users.noreply.github.com>
2022-04-23 12:38:29 -0400

    Update Littguy.java
    I jazzed up the movement for Littguy, its weird but for the game I have in mind it will work

--- Littguy.java
+++ Littguy.java
@@ -11,18 +11,17 @@ import java.awt.event.KeyEvent;
 public class Littguy {
     public int LitVolVer; //The class littguy's verticle volocity
     public int LitVolHor; //The class littguy's horizontal volocity
-    int slow;
     private BufferedImage image;
     private Point pos;
     public Littguy() {
         loadImage();
-        pos = new Point(100, 100);
+        pos = new Point(0, 0);
     }
 
 
     private void loadImage() {    
         try {
-            image = ImageIO.read(new File("images/littguy.png"));
+            image = ImageIO.read(new File("images/littguy.png")); //50 by 50 pixels
         } catch (IOException exc) {
             System.out.println("Error opening image file: " + exc.getMessage());
         }
@@ -38,61 +37,96 @@ public class Littguy {
 
     }
 
-    public void keyPressed(KeyEvent e) {
 
-        slow = 0;
-        int key = e.getKeyCode();
-                if (key == KeyEvent.VK_UP) {
-                    LitVolVer--; 
-        }
-        if (key == KeyEvent.VK_RIGHT) {
-                    LitVolHor++;
-        }
-        if (key == KeyEvent.VK_DOWN) {
-                    LitVolVer++;
-        }
-        if (key == KeyEvent.VK_LEFT) {
-                    LitVolHor--;
-        }
-
+   public void keyPressed(KeyEvent e) {
+       final int KickSpeed = 5;
+       int key = e.getKeyCode();
+//The Code below is just giving an initial kick of speed
+       if (LitVolVer > KickSpeed * -1) {
+       if (key == KeyEvent.VK_UP) {
+                   LitVolVer = LitVolVer - KickSpeed; 
+       }
     }
-
-    public void keyReleased(KeyEvent e) {
-        slow = 1;
+       if (LitVolHor < KickSpeed) {
+       if (key == KeyEvent.VK_RIGHT) {
+                   LitVolHor = LitVolHor + KickSpeed;
+       }
     }
-
-
+       if (LitVolVer < KickSpeed) {
+       if (key == KeyEvent.VK_DOWN) {
+                   LitVolVer = LitVolHor + KickSpeed;
+       }
+    }
+       if (LitVolHor > KickSpeed * -1) {
+       if (key == KeyEvent.VK_LEFT) {
+                   LitVolHor = LitVolHor - KickSpeed;
+       }
+    }
+//This code is the acceleration/main-movement
+       if (key == KeyEvent.VK_UP) {
+                   LitVolVer--; 
+       } else if  (key == KeyEvent.VK_W) {
+                   LitVolVer--; 
+       }
+       if (key == KeyEvent.VK_RIGHT) {
+                   LitVolHor++;
+       } else if  (key == KeyEvent.VK_D) {
+                   LitVolHor++;
+       }
+       if (key == KeyEvent.VK_DOWN) {
+                   LitVolVer++;
+       } else if  (key == KeyEvent.VK_S) {
+                   LitVolVer++;
+       }
+       if (key == KeyEvent.VK_LEFT) {
+                   LitVolHor--;
+       } else if  (key == KeyEvent.VK_A) {
+                   LitVolHor--;
+       }
+   }
+//the code within tick is constantly being activitated (or should be)
     public void tick() {
         pos.translate(LitVolHor, LitVolVer);  
-        if (slow == 1) {          
-        slow();
+        final int LGSpeedLimit = 20;
+//limit speed
+        if (LitVolHor < LGSpeedLimit * -1) {
+            LitVolHor = LGSpeedLimit * -1;
         }
-
-    
+        if (LitVolVer < LGSpeedLimit * -1) {
+            LitVolVer = LGSpeedLimit * -1;
+        }
+        if (LitVolHor > LGSpeedLimit) {
+            LitVolHor = LGSpeedLimit;
+        }
+        if (LitVolVer > LGSpeedLimit) {
+            LitVolVer = LGSpeedLimit;
+        }
+//slow down
+        if (LitVolHor < 0) {
+            LitVolHor = LitVolHor + 1;
+        } else if (LitVolHor > 0) {
+            LitVolHor = LitVolHor - 1;
+        }
+        if (LitVolVer < 0) {
+            LitVolVer = LitVolVer + 1;
+        } else if (LitVolVer > 0) {
+            LitVolVer = LitVolVer - 1;
+        }
+//collision boarder
         if (pos.x < 0) {
             pos.x = 0;
-        } else if (pos.x >= 2000) {
-            pos.x = pos.x - 1;
+        } else if (pos.x > 1850) {
+            pos.x = 1850;
         }
         if (pos.y < 0) {
             pos.y = 0;
-        } else if (pos.y >= 1000) {
-            pos.y = pos.y - 1;
-               }
+        } else if (pos.y > 950) {
+            pos.y = 950;
+         }
+//collision on obstacles
     }
 
-    public void slow() {
-        if (LitVolHor < 0) {
-            LitVolHor++;
-        } else if (LitVolHor > 0) {
-            LitVolHor--;
-        }
-        if (LitVolVer < 0) {
-            LitVolVer++;
-        } else if (LitVolVer > 0) {
-            LitVolVer--;
-        }
-    }
+
     public Point getPos() {
         return pos;
     }
