51b0ccd1453e5c1d413b0b12774ac7bd78967731
hotpig <93409262+MicahMaphet@users.noreply.github.com>
2022-04-23 12:31:54 -0400

    Update Screen.java
    I am making it render walls for the character

--- Screen.java
+++ Screen.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
 
@@ -8,11 +9,13 @@ public class Screen extends JPanel implements ActionListener, KeyListener {
     private Timer timer;
     private Littguy littguy;
     public int Screensize = 500;
+    private ArrayList<Obstacle> obstacles;
 
     public Screen() {
       setPreferredSize(new Dimension(Screensize, Screensize));
       setBackground(new Color(232, 232, 232));
       littguy = new Littguy();
+      obstacles = drawObstacles(200, 300, 200, 200);
       timer = new Timer(DELAY, this);
       timer.start();
   }
@@ -24,10 +27,32 @@ public class Screen extends JPanel implements ActionListener, KeyListener {
   @Override
   public void paintComponent(Graphics g) {
       super.paintComponent(g);
+      for (Obstacle obstacle : obstacles){
+        obstacle.draw(g, this);
+      }
       littguy.draw(g, this);
+
       Toolkit.getDefaultToolkit().sync();
   }
 
+  public void drawObstacles(int x1, int x2, int y1, int y2) {
+    if (x1 < x2){
+    for (int i = 0; i < Math.abs(x1 - x2) / 50; i++) {
+      ManageOL(50 * i + x1, y1);
+    }
+  
+    } else if (x1 > x2) {
+      for (int i = 0; i < Math.abs(x2 - x1) / 50; i++) {
+        ManageOL(50 * i + x1, y1); 
+    }
+  }
+}
+
+ public ArrayList<Obstacle> ManageOL(int x_, int y_) {
+  ArrayList<Obstacle> ObstacleList = new ArrayList<>();
+  ObstacleList.add(new Obstacle(x_, y_));
+  return ObstacleList;
+ }
   @Override
   public void keyTyped(KeyEvent e) {
   }
@@ -40,7 +65,6 @@ public class Screen extends JPanel implements ActionListener, KeyListener {
 
   @Override
   public void keyReleased(KeyEvent e) {
-    littguy.keyReleased(e);
   }
 }
 
